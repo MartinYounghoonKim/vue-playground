5b18dcd8bc2741ab61bf1c321eead3e2446e2743
martinyounghoonkim <hoons0131@gmail.com>
2018-05-18 13:22:38 +0900

    Testing webpack dist file

---
+++ dist/build.1.js
@@ -0,0 +1,23 @@
+/**
+ * @description defineProperty를 이용해서 '__webpack_exports__' 에 '__esModule' property를 추가
+ * @description 출처: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+ */
+var o = {}; // 새로운 객체를 생성한다.
+// 데이터 속성기술로 defineProperty를 이용해 속성을 추가한다
+Object.defineProperty(o, 'a', {
+    value: 37,
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__a__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__b__ = __webpack_require__(2);
+
+
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__a__["a" /* default */]);
+console.log(__WEBPACK_IMPORTED_MODULE_1__b__["a" /* default */]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvbWFpbi5qcz8zNDc5Il0sIm5hbWVzIjpbImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7OztBQUFBO0FBQ0E7O0FBRUFBLFFBQVFDLEdBQVIsQ0FBWSxtREFBWjtBQUNBRCxRQUFRQyxHQUFSLENBQVksbURBQVoiLCJmaWxlIjoiMC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBhIGZyb20gJy4vYSc7XG5pbXBvcnQgYiBmcm9tICcuL2InO1xuXG5jb25zb2xlLmxvZyhhKTtcbmNvbnNvbGUubG9nKGIpO1xuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL3NyYy9tYWluLmpzIl0sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceURL=webpack-internal:///0
